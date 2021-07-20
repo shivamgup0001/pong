46acef08f68ec7d311b17ca5814fe6ac8224d5eb
shivamgup0001 <shivamgup00001@gmail.com>
2021-07-20 16:13:36 +0530

    second commit

--- app/src/main/java/com/example/pong/views/custom_view.java
+++ app/src/main/java/com/example/pong/views/custom_view.java
@@ -2,12 +2,15 @@ package com.example.pong.views;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.graphics.Typeface;
+import android.os.Build;
 import android.os.CountDownTimer;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
@@ -17,8 +20,11 @@ import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 
 import com.example.pong.R;
+
+import static android.content.Context.MODE_PRIVATE;
 
 public class custom_view extends View {
 
@@ -26,12 +32,12 @@ public class custom_view extends View {
     private Rect rect1;
     private Paint paint;
     private Paint paint1;
+    private Paint paint3;
     private int counter=0;
-    private Bitmap bmp;
     private int x1=6;
     private int y1=6;
+    private int check=0;
     private int score=0;
-
 
 
     public custom_view(Context context) {
@@ -52,6 +58,7 @@ public class custom_view extends View {
         init (attrs);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public custom_view(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
 
@@ -62,62 +69,71 @@ public class custom_view extends View {
         rect=new Rect();
         rect1=new Rect();
         paint =new Paint();
-paint1=new Paint();
+        paint1=new Paint();
         paint.setColor(Color.GREEN);
-paint1.setColor(Color.YELLOW);
+        paint1.setColor(Color.YELLOW);
+        paint3=new Paint();
+
+        paint3.setColor(Color.WHITE);
+        paint3.setTextSize(80);
+        paint3.setStyle(Paint.Style.FILL);
 
     }
+
+
     @Override
     protected void onDraw (Canvas canvas) {
         if (counter == 0) {
             rect.left = getWidth() / 2 - 150;
-            rect.top = getHeight() - 50;
+            rect.top = getHeight() - 40;
             rect.right = rect.left + 300;
             rect.bottom = getHeight();
             rect1.left = getWidth() / 2 - 20;
-            rect1.right = rect1.left + 40;
+            rect1.right = rect1.left + 50;
             rect1.top = getHeight() / 2 - 20;
-            rect1.bottom = rect1.top + 40;
+            rect1.bottom = rect1.top + 50;
         }
 
-        ball(canvas);
-
-
-
+       // ball(canvas);
         canvas.drawColor(Color.BLACK);
         canvas.drawRect(rect, paint);
         canvas.drawRect(rect1, paint1);
-
-
-    }
-    public void ball(Canvas c)
-    {
-
-        if (rect1.left < 0 || rect1.right >= c.getWidth())
+        canvas.drawText("Score:" + score, canvas.getWidth()/3+20, canvas.getHeight()/14, paint3);
+        if (rect1.left < 0 || rect1.right >= canvas.getWidth())
             x1 *= -1;
         if (rect1.top < 0)
             y1*=-1;
-        if(rect1.left>=rect.left&&rect1.right<=rect.right) {
-            if (rect1.bottom >= (c.getHeight() - 50)) {
-                y1 *= -1;
-                score++;
-                if(rect1.bottom>=c.getHeight())
-                    c.drawColor(Color.WHITE);
-            }
+        if((rect1.top>=(canvas.getHeight()-80))&&(rect1.left>=rect.left&&rect1.right<=rect.right))
+        {
+            y1*=-1;
+            score++;
         }
-        if(rect1.bottom>=c.getHeight())
-            c.drawColor(Color.WHITE);
+        if(rect1.top>=canvas.getHeight()-60)
+        {
+            check=1;
+            SharedPreferences sh = getContext().getSharedPreferences("pong", Context.MODE_PRIVATE);
+            int a = sh.getInt("highscore", 0);
+            if(a<score) {
+                SharedPreferences.Editor myEdit = sh.edit();
+                myEdit.putInt("highscore", score);
+                canvas.drawText("High Score:" +score, canvas.getWidth()/4, canvas.getHeight()/6, paint3);
+                myEdit.apply();
+            }
+            else
+            {
+                canvas.drawText("High Score:" + a, canvas.getWidth()/4, canvas.getHeight()/6, paint3);
+            }
 
-
-
+        }
 
         rect1.left += x1;
-        rect1.right = rect1.left + 40;
+        rect1.right = rect1.left + 50;
         rect1.top += y1;
-        rect1.bottom = rect1.top + 40;
-
-        postInvalidate();
+        rect1.bottom = rect1.top + 50;
+        if(check==0)
+            postInvalidate();
     }
+
     @Override
     public boolean onTouchEvent(MotionEvent event)
     {
@@ -142,7 +158,7 @@ if(x>150&&x<(getWidth()-150)) {
         rect.right = rect.left + 300;
         rect.bottom = getHeight();
         counter++;
-
+if(check==0)
         postInvalidate();
         return true;
     }
@@ -153,5 +169,5 @@ if(x>150&&x<(getWidth()-150)) {
         }
         return value;
     }
-
 }
+
